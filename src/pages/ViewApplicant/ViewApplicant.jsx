a22d0df5e0f8af5463d02332f40794f51f857ad8
Mdjakaria53 <kjakaria53@gmail.com>
2025-05-31 22:04:07 +0900

    applicant stats updata successfully

--- src/pages/ViewApplicant/ViewApplicant.jsx
+++ src/pages/ViewApplicant/ViewApplicant.jsx
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 import { useLoaderData } from 'react-router';
 
 const ViewApplicant = () => {
     const applicants = useLoaderData();
     console.log(applicants);
+
+    const handleOnchange = (e, app_id) => {
+        const stats = e.target.value;
+        console.log(stats, app_id);
+        
+        axios.patch(`http://localhost:3000/application/${app_id}`, { stats })
+            .then(res => {
+                if (res.data.modifiedCount) {
+                return alert('update successfully')
+            }
+            })
+            .catch(error => {
+            console.log(error);
+        })
+    }
     return (
       <div className="overflow-x-auto">
         <table className="table">
@@ -14,16 +30,26 @@ const ViewApplicant = () => {
               <th>email</th>
               <th>address</th>
               <th>resume</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
             {applicants.map((applicant) => (
-              <tr key={applicant._id}>
-                <td>{applicant.name}</td>
+              <tr key={applicant?._id}>
+                <td>{applicant?.name}</td>
                 <td>{applicant?.email}</td>
                 <td>{applicant?.address}</td>
-                <id>{applicant?.resume}</id>
+                <td>{applicant?.resume}</td>
+                <td>
+                  <select onChange={(e)=> handleOnchange(e,applicant?._id)} defaultValue={applicant?.stats} className="select">
+                    <option disabled={true}>Update Stats</option>
+                    <option>Pending</option>
+                    <option>Interview</option>
+                    <option>Hired</option>
+                    <option>Reject</option>
+                  </select>
+                </td>
               </tr>
             ))}
           </tbody>
